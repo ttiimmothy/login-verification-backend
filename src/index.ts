152d1f469c2e81ad939da12f73218b8a9c0e97c5
ttiimmothy <dinosauli2006@gmail.com>
2024-09-07 22:18:22 -0400

    rollback

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ app.post('/api/verify', (req, res) => {
   res.status(200).json({ message: 'Verification successful' });
 });
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
